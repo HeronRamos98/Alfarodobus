3f5d5bc812cddde168cf44418c7ae16b631dfb2b
Heron Ramos <heron@ALFARODOBUS.local>
2024-12-06 10:40:22 -0300

    Configurando o framework Spring Boot

---
+++ src/main/java/chamados/Alfarodobus/AlfarodobusApplication.java
@@ -0,0 +1,13 @@
+package chamados.Alfarodobus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfarodobusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfarodobusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chamados/Alfarodobus/AlfarodobusApplicationTests.java
@@ -0,0 +1,13 @@
+package chamados.Alfarodobus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfarodobusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
